1f68259865e54fc57f332ab7930688dd61bc29fe
Anindya <banindya99@gmail.com>
2022-07-28 19:09:18 +0600

    websiteComplete

--- app.js
+++ app.js
@@ -15,3 +15,72 @@ function bounce1() {
     count= count + 1;
     document.getElementById('counter').innerText="+"+count;
 }
+function url1(){
+    window.location.assign("https://909jump.com/rentals/jumpers/glass-slipper-castle/")
+}
+function url2(){
+    window.location.assign("https://909jump.com/rentals/jumpers/circus-act-bounce-house/")
+}
+function url3(){
+    window.location.assign("https://909jump.com/rentals/dry-slides/california-dreaming-dual-lane-water-slide/")
+}
+function url4(){
+    window.location.assign("https://909jump.com/rentals/water-slides/tiger-king-falls/")
+}
+function url5(){
+    window.location.assign("https://909jump.com/rentals/water-slides/tropical-thunderstorm/")
+}
+function url6(){
+    window.location.assign("https://909jump.com/rentals/water-slides/tropical-thunderstorm/")
+}
+function url7(){
+    window.location.assign("")
+}
+function url8(){
+    window.location.assign("https://909jump.com/rentals/combos-jumpers-w-slides/camelot-combo/")
+}
+function url9(){
+    window.location.assign("https://909jump.com/rentals/combos-jumpers-w-slides/carnival-combo/")
+}
+function url10(){
+    window.location.assign("https://909jump.com/rentals/combos-jumpers-w-slides/jump-n-slide-funhouse/")
+}
+function url11(){
+    window.location.assign("https://909jump.com/rentals/concessions/snow-cone-maker/")
+}
+function url12(){
+    window.location.assign("https://909jump.com/rentals/concessions/margarita-slushy-machine/")
+}
+function url13(){
+    window.location.assign("https://909jump.com/rentals/concessions/cotton-candy-machine/")
+}
+function url14(){
+    window.location.assign("https://909jump.com/rentals/concessions/giant-dice/")
+}
+function url15(){
+    window.location.assign("")
+}
+function url16(){
+    window.location.assign("https://909jump.com/rentals/concessions/giant-jenga/")
+}
+function url17(){
+    window.location.assign("https://909jump.com/rentals/combos-jumpers-w-slides/carnival-combo/")
+}
+function url18(){
+    window.location.assign("https://909jump.com/rentals/combos-jumpers-w-slides/jump-n-slide-funhouse/")
+}
+function url19(){
+    window.location.assign("https://909jump.com/rentals/water-slides/dual-lane-multi-color-water-slide/")
+}
+function url20(){
+    window.location.assign("https://909jump.com/rentals/tables-chairs/standard-size-table/")
+}
+function url20(){
+    window.location.assign("https://909jump.com/rentals/tables-chairs/folding-chairs/")
+}
+function url21(){
+    window.location.assign("https://909jump.com/rentals/tents-canopies-misters/10x10-tent/")
+}
+function url22(){
+    window.location.assign("https://909jump.com/rentals/tents-canopies-misters/10x20-canopy/")
+}
